15782fa42fe8de6bc8f69c2802721c91276b1df1
Samuel Pereira <gmr.quest.10@gmail.com>
2021-12-01 20:39:29 -0300

    Remove a importacao do header do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 
 export default class App extends Component {
